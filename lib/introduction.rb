50f57eca0b900b2a70fb27162f207e9214f5e341
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-04-30 23:03:43 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,7 @@
 def introduction(myname)
   puts "Hi, my name is #{myname}."
 end
+
+def introduction_with_language(name, language)
+  puts "Hi my name is #{myname} and I am learning to program in #{language}."
+end
